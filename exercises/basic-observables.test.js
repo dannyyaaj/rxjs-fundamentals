f5279e9fac5e20323d096785c59d1e2ba5672bd4
Danny Yang <dannyyaaj@gmail.com>
2022-03-30 21:13:21 -0500

    complete basic test

--- exercises/basic-observables.test.js
+++ exercises/basic-observables.test.js
@@ -2,8 +2,10 @@ import { from, of } from 'rxjs';
 
 describe('Basic Observables', () => {
   describe(of, () => {
-    it.skip('should create an observable from its arguments', () => {
+    it('should create an observable from its arguments', () => {
       const result = [];
+      const observable$ = of(1, 2, 3, 4);
+      observable$.subscribe((value) => result.push(value))
 
       expect(result).toEqual([1, 2, 3, 4]);
     });
@@ -12,6 +14,9 @@ describe('Basic Observables', () => {
   describe(from, () => {
     it.skip('should create an observable', () => {
       const result = [];
+
+      const observable$ = from(1,2,3,4);
+      observable$.subscribe((value) => result.push(value))
 
       expect(result).toEqual([1, 2, 3, 4]);
     });
