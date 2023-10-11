7dbf300d99fcfe41391a56df998b787ec8f0910a
Danny Yang <dannyyaaj@gmail.com>
2023-10-11 11:26:59 -0500

    work on mergeing timelines activity

--- applications/merging-timelines/script.js
+++ applications/merging-timelines/script.js
@@ -6,4 +6,14 @@ import {
   pauseButton,
   setStatus,
   bootstrap,
+  clearButton,
 } from './utilities';
+
+const start$ = fromEvent(startButton, 'click').pipe(mapTo(true))
+const pause$ = fromEvent(pauseButton, 'click').pipe(mapTo(false))
+
+//give it default value of false before anything is emitted
+const isRunning$ = merge(start$, pause$).pipe(startWith(false))
+
+isRunning$.subscribe((value) => { console.log('is running', value) })
+
